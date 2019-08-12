b182a2d0e1c915b8940f1156227f634a98f09dcc
ykubai <ykubai@s-pro.io>
2019-08-12 15:41:49 +0300

    add sonar-project.properties

--- tests/Unit/ControllerTest.php
+++ tests/Unit/ControllerTest.php
@@ -17,4 +17,13 @@ class ControllerTest extends TestCase
     {
         $this->assertTrue((new TestController)->testingData() == [1,2,3]);
     }
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testTestingData2()
+    {
+        $this->assertTrue((new TestController)->testingData2() == [1,2,3]);
+    }
 }
